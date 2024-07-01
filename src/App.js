bb0ac3bf1437e96560b77443812de72d0e033d83
harshit jain <harshitjainmahi@gmail.com>
2024-07-01 17:38:16 +0530

    adding bookpen images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from "react";
 import WhyMoofli from "./WhyMoofli";
 import Header from "./Header";
 import Reviews from "./Reviews";
+import Bookpen from "./Bookpen";
 import './App.css';
 function App() {
   return (
     <div className="App">
+      <Bookpen />
       <Header/>
       <div className="main">
         <WhyMoofli/>
---
+++ src/Bookpen.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import rectangle2 from './rectangles/Rectangle 2.png'
+import rectangle3 from './rectangles/Rectangle 3.png'
+import rectangle4 from './rectangles/Rectangle 4.png'
+import rectangle5 from './rectangles/Rectangle 5.png'
+import bookpen from './book-pen.png'
+const Bookpen = () => {
+  return (
+    <div className="new-element">
+      <div className='rectangle-images'>
+        <img src={rectangle2} />
+        <img src={rectangle3} />
+        <img src={rectangle4} />
+        <img src={rectangle5} />
+      </div>
+      <div className='bookpen-images'>
+        <img src={bookpen}/>
+      </div>
+    </div>
+  )
+}
+
+export default Bookpen
