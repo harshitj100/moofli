396c41bd9802cfcf08fa40d95e2a4e173120bd2e
harshit jain <harshitjainmahi@gmail.com>
2024-07-02 21:47:55 +0530

    adding responsivity - 01 1440px

--- src/Reviews.js
+++ src/Reviews.js
@@ -5,12 +5,6 @@ const Reviews = () => {
       <div className='moofli-desc'>
         Where minds crunch together just like peanuts..
       </div>
-      <div className='like-count'>
-        <div className='value'>4.5k+ O</div>
-        <div className='value'>3.6k+ O</div>
-        <div className='value'>234 O</div>
-        <div className='value'>6.7k+ O</div>
-      </div>
 
       <div className='join-moofli-statement'>
         Join the Moofli to connect with others and discover yourself
