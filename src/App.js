3ce8dd44e680d7baa57332d62af3ea3728e3eb8a
harshit jain <harshitjainmahi@gmail.com>
2024-07-03 18:01:28 +0530

    react-router

--- src/App.js
+++ src/App.js
@@ -7,20 +7,26 @@ import BookPenText from "./BookPenText";
 import Star from "./Star";
 import JoinUs from "./JoinUs";
 import './App.css';
+import { Link } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Bookpen />
-      <BookPenText />
-      <Star />
-      <Header/>
-      <div className="main">
-        <WhyMoofli/>
-        <Reviews />
-      </div>     
-      <JoinUs />
-    </div>
+    <Router>
+      <div className="App">
+        <Bookpen />
+        <BookPenText />
+        <Star />
+        <Header/>
+        <div className="main">
+          <WhyMoofli/>
+          <Reviews />
+        </div>     
+        <Link to="https://youtube.com">
+          <JoinUs />
+        </Link>
+      </div>
+    </Router>
   );
 }
 
